9d5d699e5a845d71169cc3c17b885f7a12d1cd07
Macdeo <akinbiyi.adeola@gmail.com>
2023-03-13 23:29:25 +0100

    modified:   javascript/index.js

--- javascript/index.js
+++ javascript/index.js
@@ -9,8 +9,14 @@ console.log("a is " + a);
 console.log("b is " + b);
 
 
-var names = 'angela';
+var names = 'angELa';
 var capName = names.slice(0,1)
 capName = capName.toUpperCase();
 var reNames = names.slice(1,6)
+reNames = reNames.toLowerCase();
 console.log('Hello ' + capName + reNames );
+
+var dogAge = 7;
+var humanAge = (dogAge - 2) * 4 + 21;
+
+console.log(humanAge);
