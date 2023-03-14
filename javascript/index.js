e157b52666617f43990eed4ca2fb1c1f04d2ec29
Macdeo <akinbiyi.adeola@gmail.com>
2023-03-14 23:34:02 +0100

    modified:   javascript/index.js

--- javascript/index.js
+++ javascript/index.js
@@ -10,13 +10,33 @@ console.log("b is " + b);
 
 
 var names = 'angELa';
-var capName = names.slice(0,1)
+var capName = names.slice(0, 1)
 capName = capName.toUpperCase();
-var reNames = names.slice(1,6)
+var reNames = names.slice(1, 6)
 reNames = reNames.toLowerCase();
-console.log('Hello ' + capName + reNames );
+console.log('Hello ' + capName + reNames);
 
 var dogAge = 7;
 var humanAge = (dogAge - 2) * 4 + 21;
 
 console.log('This is your dog age: ' + humanAge);
+
+
+
+
+calAge(56);
+
+
+
+
+
+
+function calAge(age) {
+    var totalweeks = 52 * 90;
+    var weeksLeft = totalweeks - (age * 52);
+    var totaldays = 365 * 90;
+    var daysLeft = totaldays - (age * 365);
+    var totalmonths = 12 * 90;
+    var monthsLeft = totalmonths - (age * 12);
+    console.log('You have ' + daysLeft + ' days, ' + weeksLeft + ' weeks, ' + monthsLeft + ' months left');
+}    
